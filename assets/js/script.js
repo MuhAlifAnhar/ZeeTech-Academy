8692273329c702a79fdc9eed101b342861930710
Muh. Alif Anhar <sultankautsar21@gmail.com>
2025-12-14 06:41:21 +0800

    feat: menambahkan fitur testimoni

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,4 +96,31 @@ document.addEventListener('DOMContentLoaded', function() {
         once: true,
         offset: 100
     });
+
+    // Initialize Swiper
+    var swiper = new Swiper(".testimonial-slider", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
+    });
 });
